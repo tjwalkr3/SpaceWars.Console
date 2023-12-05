ef19e20dc5fe8ff545de00b1372f19c5005a30e8
Breanna Nesbit <brebrenesbit02@gmail.com>
2023-12-05 09:25:11 -0700

    notes

--- SWConsole/Program.cs
+++ SWConsole/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//enter url to sent request to
+//prompt for name
+// Join game and return player token and ship location, board info (saving these)
+
+
+// Moving (need to make move)
+// when certain key is pressed call the move function with the direction to move and update location of ship
