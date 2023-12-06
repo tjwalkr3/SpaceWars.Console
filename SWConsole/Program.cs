4c5e2d7bc8f83b06aad506dc81bc1a7bb6bd5bea
Zackery Anderson <zackatack108@gmail.com>
2023-12-06 09:22:45 -0700

    Added Loop so console program doesn't end

--- SWConsole/Program.cs
+++ SWConsole/Program.cs
@@ -3,45 +3,55 @@ using System.Net.Http;
 
 namespace SpaceWarsServices;
 
-    class Program
+class Program
+{
+    static async Task Main()
     {
-        static async Task Main()
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Please enter the URL to access Space Wars");
+        // Enter URL to send requests
+        var url = Console.ReadLine();
+
+        bool exitGame = false;
+
+        using (HttpClient httpClient = new HttpClient())
         {
-            Console.WriteLine("Hello, World!");
+            httpClient.BaseAddress = new Uri(url);
 
-            Console.WriteLine("Please enter the URL to access Space Wars");
-            // Enter URL to send requests
-            var url = Console.ReadLine();
+            // Create an instance of your ApiService (assuming ApiService takes HttpClient in its constructor)
+            var service = new ApiService(httpClient);
 
-            using (HttpClient httpClient = new HttpClient())
+            try
             {
-                httpClient.BaseAddress = new Uri(url);
+                // Prompt for name
+                Console.WriteLine("Please enter your name");
+                var username = Console.ReadLine();
+                var results = await service.JoinGameAsync(username);
 
-                try
-                {
-                    // Create an instance of your ApiService (assuming ApiService takes HttpClient in its constructor)
-                    var service = new ApiService(httpClient);
+                // Join game and return player token and ship location, board info (saving these)
+                // You can call methods on the service to handle this part of the logic
+                Console.WriteLine($"Token:{results.Token}");
+                Console.WriteLine($"Ship located at: {results.StartingLocation}, Game State is: {results.GameState}, Board Demensions: {results.BoardWidth}, {results.BoardHeight}, ");
 
-                    // Prompt for name
-                    Console.WriteLine("Please enter your name");
-                    var username = Console.ReadLine();
-                    var results = await service.JoinGameAsync(username);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
 
-                    Console.WriteLine($"Ship located at: {results.StartingLocation}, Game State is: {results.GameState}, Board Demensions: {results.BoardWidth}, {results.BoardHeight}, ");
-                    // Join game and return player token and ship location, board info (saving these)
-                    // You can call methods on the service to handle this part of the logic
+            while (!exitGame)
+            {
+                // Moving (need to make a move)
+                // When a certain key is pressed, call the move function with the direction to move and update the location of the ship
+                // You can handle this part of the logic using methods in the service as well
 
-                    // Moving (need to make a move)
-                    // When a certain key is pressed, call the move function with the direction to move and update the location of the ship
-                    // You can handle this part of the logic using methods in the service as well
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error: {ex.Message}");
-                }
             }
         }
-    }
 
-    // Assuming ApiService class structure
+    }
+}
+
+// Assuming ApiService class structure
    
